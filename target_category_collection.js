cef4ba056bbbf3e6fdd45c686690a0b63504ee00
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-08 21:27:33 +0900

    hotfix typo

--- target_category_collection.js
+++ target_category_collection.js
@@ -18,7 +18,7 @@ function writeCSV(res) {
 
   try {
     const csv = json2csv(res, { header });
-    fs.appendFile("target_category_collection_" + formatterd + ".csv", csv);
+    fs.appendFile("target_category_collection_" + formatted + ".csv", csv);
   } catch (err) {
     console.error(err);
   }

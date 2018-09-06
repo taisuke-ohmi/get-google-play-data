577ed753209fa77da488feb743833c8b6159bd6f
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-09-07 02:35:13 +0900

    target_categoryを修正

--- target_category.js
+++ target_category.js
@@ -7,43 +7,35 @@ const sleep = require('sleep');
 
 var filename = "output.csv";
 var fields = ["title","installs","minInstalls","developer","developerEmail","developerWebsite","genre","genreId","appId","url"];
-var categories = [
-//  "SPORTS"
-//  "DATING"
-//  "BUSINESS"
-//  "HEALTH_AND_FITNESS"
-//  "PHOTOGRAPHY"
-//  "TOOLS"
-//  "BOOKS_AND_REFERENCE"
-//  "AUTO_AND_VEHICLES"
-//  "EDUCATION"
-//  "MEDICAL"
-//  "PRODUCTIVITY"
-  "HOUSE_AND_HOME"
-];
+var category = "SPORTS";
+var progress = [];
 
-var start = -1;
 try {
-  fs.appendFile(filename, "CATEGORY NAME: " + categories[0] + "\n");
+  fs.appendFile(filename, "CATEGORY NAME: " + category + "\n", fileError);
 } catch (err) {
   console.error(err);
 }
 
-console.log(categories[0]);
-gplaySearch()
-  .then(gplaySearch)
-  .then(gplaySearch)
-  .then(gplaySearch)
-  .then(gplaySearch)
+console.log("CATEGORY NAME: " + category);
+
+gplayList()
+  .then(gplayList)
+  .then(gplayList)
+  .then(gplayList)
+  .then(gplayList)
+  .then(gplayList)
+  .then(gplayList)
+  .then(gplayList)
+  .then(gplayList)
+  .then(gplayList)
   .catch(console.log)
 
-function gplaySearch() {
-  start++
-  console.log(start);
+function gplayList() {
+  console.log(progress.length + " times.");
   return gplay.list({
-    category: categories[0],
-    num: 100, // set number to get
-    start: (start * 100),
+    category: category,
+    num: 50, // set number to get
+    start: (progress.length * 50),
     lang: "ja",
     country: "jp",
     fullDetail: true,
@@ -53,12 +45,19 @@ function gplaySearch() {
 
 function writeCSVNoHeader(res) {
   try {
-    console.log(res.length);
+    console.log(res.length + " apps fetch.");
     const csv = json2csv(res, { fields, header:false });
-    fs.appendFile(filename, csv);
-    fs.appendFile(filename, "\n");
+    fs.appendFile(filename, csv, fileError);
+    fs.appendFile(filename, "\n", fileError);
+    progress.push(1);
   } catch (err) {
     console.error(err);
   }
 }
-*/
+
+function fileError(err) {
+  if (err) {
+    console.log(err);
+  }
+  return false;
+}
